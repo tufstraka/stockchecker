9814fb2447c1888e80f48b7b32ba37b098814afc
kadimakeith <keithkadima@gmail.com>
2024-07-08 10:32:52 +0000

    fix: import suites and test from mocha

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,6 +2,8 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const { suite, test } = require('mocha');
+
 
 chai.use(chaiHttp);
 
